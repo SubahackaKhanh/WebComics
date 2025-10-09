0de2cf26a2d6c1160f6c7d59c667c10d13f35973
SubahackaKhanh <nguyenvanthienmobile@gmail.com>
2025-10-09 10:20:09 +0700

    add: AuthLayout and refactor router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <Header />
   <router-view/>
-  <!-- <Horizontal_items/>
-  <List_items/> -->
-  <Footer />
 </template>
 
 <script setup>
   import './assets/main.css';
-  import Header from './components/global/Header.vue';
-  import Footer from './components/global/Footer.vue';
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Genres from './components/pages/Genres.vue'
 import Home from './components/pages/Home.vue'
+import AuthLayout from './layouts/AuthLayout.vue'
+import SignUp from './components/pages/SignUp.vue'
+
 
 const routes = [
     { path: '/', component: Home},
     { path: '/genres', component: Genres },
+    { path: '/signup', component: AuthLayout, children: [{ path: '', component: SignUp}]},
 ]
 
 const router = createRouter({

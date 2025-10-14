52a0324b826a8c068cb8139fc9fd7604ce077500
SubahackaKhanh <nguyenvanthienmobile@gmail.com>
2025-10-14 20:29:10 +0700

    feat: Add Details common components

---
+++ src/components/common/ItemDetails.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="details-container">
+        <h1> ONE PUNCH MAN</h1>         <!-- <h1> {{ items.name }}</h1> -->
+        {{ details }}
+    </div>
+</template>
+
+<script setup>
+    const details = "One Punch Man là một series anime/manga hành động – hài hước xoay quanh Saitama, một anh chàng hói bình thường nhưng lại sở hữu sức mạnh phi thường: hạ gục bất kỳ đối thủ nào chỉ với một cú đấm. Điều này khiến cuộc sống làm anh hùng của anh trở nên... nhàm chán. Không còn cảm giác hồi hộp hay thử thách, Saitama bắt đầu hành trình tìm kiếm một đối thủ xứng tầm."
+</script>
+
+<style scoped src="@/css/itemdetails.css"></style>
--- src/router.js
+++ src/router.js
@@ -5,10 +5,13 @@ import AuthLayout from './layouts/AuthLayout.vue'
 import SignUp from './components/pages/SignUp.vue'
 import MainLayout from './layouts/MainLayout.vue'
 import Login from './components/pages/Login.vue'
+import ItemDetailsLayout from './layouts/ItemDetailsLayout.vue'
+import ItemDetails from './components/common/ItemDetails.vue'
 
 const routes = [
     { path: '/', component: MainLayout, children: [{ path: '', component: Home}, { path:'/genres', component: Genres }]},
     { path: '/signup', component: AuthLayout, children: [{ path: '', component: SignUp}, { path: '/login', component:Login}]},
+    { path: '/details', component: ItemDetailsLayout, children: [{ path: '', component: ItemDetails}]}
 ]
 
 const router = createRouter({

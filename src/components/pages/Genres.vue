c97cba2eb822ea9ed63e684ad8389133f1323cb0
SubahackaKhanh <nguyenvanthienmobile@gmail.com>
2025-10-14 08:29:14 +0700

    feat: Add List-items to GenresPage

--- src/components/pages/Genres.vue
+++ src/components/pages/Genres.vue
@@ -3,12 +3,14 @@
     :genres="tags"
     v-model:selectedTags="selectedTags"
   />
+  <List_items/>
 </template>
 
 <script setup>
 import TagsContainer from '../main-items/TagsContainer.vue'
 import { tags } from '@/js/module/list-tags.js'
 import { ref } from 'vue'
+import List_items from '../main-items/List_items.vue'
 
 const selectedTags = ref([])
 </script>

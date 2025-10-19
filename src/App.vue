2ea07c7971b412ba1b55aa373cafa2079edaa0a5
SubahackaKhanh <nguyenvanthienmobile@gmail.com>
2025-10-19 12:33:46 +0700

    refactor: app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <Loading v-if="loading.global" />
+  <Loading />
+  <RateLimitWarning />
   <router-view />
 </template>
 
 <script setup>
 import { useLoadingStore } from '@/js/composables/useLoadingStore'
 import Loading from '@/components/common/Loading.vue'
+import RateLimitWarning from '@/components/common/RateLimitWarning.vue'
 // import { storeToRefs } from 'pinia'
 
 const loading = useLoadingStore()
